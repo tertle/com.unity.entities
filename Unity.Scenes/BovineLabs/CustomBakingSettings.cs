a74ce03a92f4440bac772cb823072cd8528c175f
Timothy Raines <tim@ninjatertle.com>
2023-10-05 20:30:36 +1100

    Added a toggle to stop Closed SubScenes rebaking in play mode.

---
+++ Unity.Scenes/BovineLabs/CustomBakingSettings.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Unity.Scenes
+{
+    public static class CustomBakingSettings
+    {
+        public static bool PlayModeClosedSubSceneBaking
+        {
+            get => EditorPrefs.GetBool("BovineLabs.Entities.Streaming.SubScene.StopPlayModeClosedSubSceneBaking", true);
+            set
+            {
+                if (PlayModeClosedSubSceneBaking == value)
+                {
+                    return;
+                }
+
+                EditorPrefs.SetBool("BovineLabs.Entities.Streaming.SubScene.StopPlayModeClosedSubSceneBaking", value);
+            }
+        }
+    }
+}
+#endif
