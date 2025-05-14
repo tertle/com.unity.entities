b5b215d3faded1f08fbebce983bd3921c429ccbe
Timothy Raines <tim@ninjatertle.com>
2025-05-15 09:33:45 +1000

    Define BL_ENTITIES_CUSTOM

---
+++ Unity.Entities.Editor/EditorInitialization.cs
@@ -0,0 +1,23 @@
+﻿#if !BL_ENTITIES_CUSTOM
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build;
+
+namespace Unity.Entities.Editor
+{
+    [InitializeOnLoad]
+    class EditorInitialization
+    {
+        static readonly string k_CustomDefine = "BL_ENTITIES_CUSTOM";
+
+        static EditorInitialization()
+        {
+            var fromBuildTargetGroup = NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            var definesStr = PlayerSettings.GetScriptingDefineSymbols(fromBuildTargetGroup);
+            var defines = definesStr.Split(';').ToList();
+            defines.Add(k_CustomDefine);
+            PlayerSettings.SetScriptingDefineSymbols(fromBuildTargetGroup, string.Join(";", defines.ToArray()));
+        }
+    }
+}
+#endif
